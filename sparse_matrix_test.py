556f472921246387ddd3bdc027297a06ff19aca5
Holden Lee <holden1@mit.edu>
2013-05-15 22:33:36 -0400

    command line interface + transitivity

---
+++ sparse_matrix_test.py
@@ -0,0 +1,12 @@
+##
+##from d4d import d4d
+##import sys
+##from csc import divisi2
+##
+##if __name__=='__main__':
+##        l=[0 for i in xrange(100)]
+##        s=SparseMatrix(l,xrange(100),xrange(100))
+##        t=divisi.make_sparse([(3,1,2)])
+##        s=s+t
+##        print s
+##        
